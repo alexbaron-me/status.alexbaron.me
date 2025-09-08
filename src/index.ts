21c11ba495a362a255f619dcfe4d5002d642cb17
Alexander Baron <alexander.baron1@rwth-aachen.de>
2025-09-08 11:55:27 +0200

    chore: run biome formatting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { parse } from 'marked';
-import readme from '../README.md';
-import styles from './styles.css';
+import { parse } from "marked";
+import readme from "../README.md";
+import styles from "./styles.css";
 
-const targetBaseUrl = 'https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status';
+const targetBaseUrl =
+	"https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status";
 const readmeHtml = parse(readme) as string;
 
 const htmlTemplate = (content: string) => `<!DOCTYPE html>
@@ -18,50 +19,50 @@ const htmlTemplate = (content: string) => `<!DOCTYPE html>
 </body>
 </html>`;
 
-type Handler = ExportedHandler<Env>['fetch'];
+type Handler = ExportedHandler<Env>["fetch"];
 
 const statusCodeHandler: Handler = async (request, env, ctx) => {
 	const url = new URL(request.url);
 
 	try {
-		const clearedPath = url.pathname.replace(/^\/+|\/+$/g, '');
+		const clearedPath = url.pathname.replace(/^\/+|\/+$/g, "");
 		const statusCode = Number.parseInt(clearedPath);
 
 		if (Number.isNaN(statusCode) || statusCode < 100 || statusCode > 599) {
-			throw new Error('Invalid status code'); // Trigger catch block
+			throw new Error("Invalid status code"); // Trigger catch block
 		}
 
 		return Response.redirect(`${targetBaseUrl}/${statusCode}`, 302);
 	} catch {
-		return new Response('Invalid status code', { status: 400 });
+		return new Response("Invalid status code", { status: 400 });
 	}
-}
+};
 
 // Display project README
 const rootHandler: Handler = async (request, env, ctx) => {
 	return new Response(htmlTemplate(readmeHtml), {
 		headers: {
-			'Content-Type': 'text/html; charset=utf-8',
+			"Content-Type": "text/html; charset=utf-8",
 		},
 	});
-}
+};
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 
 		// Redirect to HTTPS in production
-		if (env.ENV === 'production' && url.protocol !== 'https:') {
-			url.protocol = 'https:';
+		if (env.ENV === "production" && url.protocol !== "https:") {
+			url.protocol = "https:";
 			return new Response(null, {
 				status: 301,
 				headers: {
-					'Location': url.toString(),
+					Location: url.toString(),
 				},
-			})
+			});
 		}
 
-		if (url.pathname.trim() === '/' || url.pathname.trim() === '') {
+		if (url.pathname.trim() === "/" || url.pathname.trim() === "") {
 			return rootHandler(request, env, ctx);
 		} else {
 			return statusCodeHandler(request, env, ctx);
